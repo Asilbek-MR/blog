553b25752e3493650280eeb353b26a8408ed2958
asilbek9919 <105420765+asilbek9919@users.noreply.github.com>
2023-08-24 14:23:47 +0500

    add admin

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u77lj5j4+!4kl#$bei158&1=!ytbn%snbfefkx+5j=mr1-do*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
